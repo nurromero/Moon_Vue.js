00b0ae08d8d40042f1e9b139383daee16e827e75
Nuriye <nuriyeerdogan38@gmail.com>
2024-03-07 07:29:08 +0100

    Vue app, now with external js file ˶ᵔ ᵕ ᵔ˶

---
+++ index.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+  data() {
+      return {
+          productName: 'Book a Cruise to the Moon',
+          productDescription: 'Cruise to the moon in our luxurious shuttle. Watch the astronauts working outside the International Space Station.',
+          
+
+
+      }
+  },
+});
